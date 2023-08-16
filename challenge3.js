ca3aedfc4a4c087129fa4b715bba9959c1f4bdfb
tshidiso-codespace <tshidisomoorosi@gmail.com>
2023-08-16 10:39:04 +0200

    undid the removal of "-" and adjusted code accordingly

--- challenge3.js
+++ challenge3.js
@@ -1,19 +1,19 @@
 const leoName = 'Leo'
 const leoSurname = 'Musvaire     '
-const leoBalance = '9394'
+const leoBalance = '-9394'
 
 const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
-const sarahBalance = '4582.2'
+const sarahBalance = '-4582.2'
 
 const divider = '----------------------------------'
 
 // Only change below this line
 const leoBalanceDec = parseFloat(leoBalance)
 const sarahBalanceDec = parseFloat(sarahBalance)
-const owed = leoBalanceDec + sarahBalanceDec
-const leo = leoName +" "+ leoSurname + "Owed: R " + leoBalanceDec.toFixed(2) + "\n"
-const sarah = sarahName + sarahSurname + " " + "Owed: R " + sarahBalanceDec.toFixed(2) + "\n\n"
+const owed = -leoBalanceDec - sarahBalanceDec
+const leo = leoName +" "+ leoSurname + "Owed: R " + -leoBalanceDec.toFixed(2) + "\n"
+const sarah = sarahName + sarahSurname + " " + "Owed: R " + -sarahBalanceDec.toFixed(2) + "\n\n"
 const total = "Total amount owed: R "
 const owedDec = owed.toFixed(2)
 const owedSep = owedDec.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
